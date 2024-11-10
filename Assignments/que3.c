f2b786dda042dee1a68f58d565952a78f4151761
Kshitij Totawar <kshitij.totawar@gmail.com>
2024-11-11 01:03:56 +0530

    question-2

---
+++ Assignments/que3.c
@@ -0,0 +1,12 @@
+//Perimeter of a rectangle.
+#include<stdio.h>
+int main(){
+    int h,w;         //RA2311030010150
+    printf("Enter the height:");
+    scanf("%d",&h);
+   printf("Enter the width:");
+     scanf("%d",&w);
+float perimeter = 2*(h+w);
+    printf("The volume of the sphere is: %f",perimeter);
+
+}
