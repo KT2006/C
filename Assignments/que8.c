e2dbbed1f082fd037cd82e796abf149f35572b37
Kshitij Totawar <kshitij.totawar@gmail.com>
2024-11-12 22:40:30 +0530

    Question 8

---
+++ Assignments/que8.c
@@ -0,0 +1,10 @@
+//Sum of three numbers with input in same line.
+#include<stdio.h>
+int main(){
+   int a,b,c,sum;        //RA2311030010150
+    printf("Enter three numbers separated by comma:");
+    scanf("%d,%d,%d",&a,&b,&c);
+    sum = a+b+c;
+    printf("The sum of the numbers is:%d",sum);
+
+}
