27cc033e94dbab6888b0e1a4a98be8adf2041659
Kshitij Totawar <kshitij.totawar@gmail.com>
2024-11-13 01:05:57 +0530

    Question 9

---
+++ Assignments/que9.c
@@ -0,0 +1,14 @@
+//Addition, subtraction, multiplication and division of two numbers.
+#include <stdio.h>
+ int main(){
+
+    int a,b;          //RA2311030010150
+    printf("enter two numbers:");
+    scanf("%d,%d",&a,&b);
+    printf(" The sum of the two numbers: %d \n", a+b);
+    printf(" The difference of the two numbers: %d \n", a-b);
+    printf("The multiplication of the two numbers: %d \n",a * b);
+    printf("The division of the two numbers: %f \n", (float) a / (float) b );
+
+ }
+
