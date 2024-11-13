724f72f5b99bcb4dcbf6b0b69c0d66e051a18194
Kshitij Totawar <kshitij.totawar@gmail.com>
2024-11-14 00:32:41 +0530

    output-2

---
+++ Assignments/predict the output/pred2.c
@@ -0,0 +1,8 @@
+//Predict the output-2
+#include <stdio.h>
+int main()
+{
+printf("\new_c_question\by");// /b shows move the cursour backwards.
+printf("\rprocoder");// /r denotes carriage returns.
+return 0;
+}
