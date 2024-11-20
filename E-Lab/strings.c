d8f374fc4c0979cb48008a75fce062a161daa838
Kshitij Totawar <kshitij.totawar@gmail.com>
2024-11-20 13:51:01 +0530

    strings.c

---
+++ E-Lab/strings.c
@@ -0,0 +1,14 @@
+// #include<stdio.h>
+// int main(){
+//  char str[90];
+// printf("Enter a string:");
+// scanf("%s",&str);
+// printf("The entered string is:%s",str);
+// for(int i=0;i<90;i++){
+//     if(str[i]=='a'||str[i]=='e'||str[i]=='i'||str[i]=='o'||str[i]='u'){
+
+//     str[i]='*';
+//     }
+// else str[i]='#';
+// }
+// }
